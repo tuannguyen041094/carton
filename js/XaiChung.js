37f791981d8e24484cb22f4146cb73e477fe555c
TienTran <tientranmac96@gmail.com>
2019-06-30 13:32:59 +0700

    optimize scroll handle

--- js/XaiChung.js
+++ js/XaiChung.js
@@ -8,15 +8,31 @@ $(document).ready(function($) {
     scroll_top_duration = 700,
     //grab the "back to top" link
     $back_to_top = $('.cd-top');
-  //hide or show the "back to top" link
-  $(window).scroll(function() {
+  function throttle(func, limit) {
+    var inThrottle;
+    return function() {
+      var args = arguments;
+      var context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(function() {
+          inThrottle = false;
+        }, limit);
+      }
+    };
+  }
+
+  function onScrollHandle() {
     $(this).scrollTop() > offset
       ? $back_to_top.addClass('cd-is-visible')
       : $back_to_top.removeClass('cd-is-visible cd-fade-out');
     if ($(this).scrollTop() > offset_opacity) {
       $back_to_top.addClass('cd-fade-out');
     }
-  });
+  }
+  //hide or show the "back to top" link
+  $(window).scroll(throttle(onScrollHandle, 100));
   //smooth scroll to top
   $back_to_top.on('click', function(event) {
     event.preventDefault();
@@ -27,17 +43,6 @@ $(document).ready(function($) {
       scroll_top_duration
     );
   });
-  // $('.nav-item.dropdown').mouseenter(function() {
-  //   $('.nav-item.dropdown').addClass('show');
-  //   $('.dropdown-menu').addClass('show');
-  // });
-
-  // $('.nav-item.dropdown')
-  //   .mouseleave(function() {
-  //     $('.nav-item.dropdown').removeClass('show');
-  //     $('.dropdown-menu').removeClass('show');
-  //   })
-  //   .mouseleave(); //trigger mouseleave to hide second div in beginning
 });
 
 // Slide
